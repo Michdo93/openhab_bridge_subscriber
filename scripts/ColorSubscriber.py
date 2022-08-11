cc22e7f2be7eaeb8f9c88b0733751c0c948d9a09
Michdo93 <michaeldoerflinger93@gmail.com>
2022-08-11 10:21:33 +0200

    Update ColorSubscriber.py

--- scripts/ColorSubscriber.py
+++ scripts/ColorSubscriber.py
@@ -30,7 +30,7 @@ class ColorSubscriber(object):
         """Handle subscriber data."""
         # Simply print out values in our custom message.
         self.data = data
-        if self.data.isbool == False:
+        if self.data.isnull == False:
             msg = "Received %s with hue %s, saturation %s and brightness %s" % (self.data.item, self.data.hue, self.data.saturation, self.data.brightness)
         else:
             msg = "Received %s with NULL" % self.data.item
